622e47aadaf9e2e0ff07bd2090ec7a8c3523cba9
Pablo <pablomoreirasantos.hp@gmail.com>
2025-01-09 13:49:04 -0300

    Aplicação Dockerizada

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -2,7 +2,7 @@ import { boot } from "quasar/wrappers";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.VITE_BASE_URL || "http://localhost:8000",
+  baseURL: process.env.VITE_BASE_URL,
   headers: {
     "Content-Type": "application/json"
   }
