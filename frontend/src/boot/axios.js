519790cd3b4acaa53bb4c13f3dcce06c2128fad6
Pablo <pablomoreirasantos.hp@gmail.com>
2025-01-16 11:36:36 -0300

    atualização no axios

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -2,7 +2,7 @@ import { boot } from "quasar/wrappers";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: 'https://locadora-pablo-back.altislabtech.com.br',
+  baseURL: 'process.env.VITE_BASE_URL',
   headers: {
     "Content-Type": "application/json"
   }
