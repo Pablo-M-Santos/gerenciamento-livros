f0fa0e4e17a50a26f2ddfb5dc666439047b540b1
Pablo <pablomoreirasantos.hp@gmail.com>
2025-01-16 10:59:14 -0300

    alteração no axios

--- frontend/src/boot/axios.js
+++ frontend/src/boot/axios.js
@@ -2,7 +2,7 @@ import { boot } from "quasar/wrappers";
 import axios from "axios";
 
 const api = axios.create({
-  baseURL: process.env.VITE_BASE_URL,
+  baseURL: 'https://locadora-pablo-back.altislabtech.com.br',
   headers: {
     "Content-Type": "application/json"
   }
